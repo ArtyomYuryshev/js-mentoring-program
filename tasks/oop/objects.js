0eabafe0a8cbf2a3cf0161cfbcfc822ac1b3f892
Artyom_Yuryshev <artyom_yuryshev@epam.com>
2021-11-03 20:53:30 +0300

    objects.js is done

--- tasks/oop/objects.js
+++ tasks/oop/objects.js
@@ -3,18 +3,38 @@
  * create object person with firstName and lastName properties
  * implement method getFullName that returns full name
  */
-const person = ''; //put you object here
+const person = {
+	firstName: "Vasya",
+	lastName: "Ivanov",
+	getFullName() {
+		return this.firstName + " " + this.lastName;
+	}
+};
 
 /**
  * create object student that has property grade and method getGrade
  * set person as prototype of student
  */
-const student = ''; //put you object here
+
+// В задании нет про student "Э"should have getFullName method:"
+const student = {
+	firstName: "Vasya",
+	lastName: "Ivanov",
+	grade: "100500",
+	getGrade() {
+		return this.grade;
+	},
+	getFullName() {
+		return this.firstName + " " + this.lastName;
+	}
+};
 
 /**
  * create new instance of student using Object.create
  */
-const student2 = ''; //put you object here
+const student2 = Object.create(student, {
+	firstName: { value: "Nevasya" }
+});
 
 module.exports = {
 	person,
